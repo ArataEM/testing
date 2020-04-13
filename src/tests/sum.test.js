86cc03576fc38cb43b4275713aaf3997ab765110
Artem Usenko <arata.em@gmail.com>
2020-04-13 21:27:31 +0300

    Update sum.test.js

--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -1,6 +1,10 @@
 import { sum } from "../sum.js";
 
 it('sums numbers', () => {
-  expect(sum(1,5)).toEqual(3);
+  expect(sum(1,2)).toEqual(3);
   expect(sum(2,2)).toEqual(4);
+});
+
+it('sums numbers with error', () => {
+  expect(sum(1,5)).toEqual(3);
 });
